385a39a8076cb7665f775bef05f0fa811ace4d66
Martijn Gonlag <martijngonlag@gmail.com>
2013-04-28 12:53:12 -0700

    Update news.py

--- news.py
+++ news.py
@@ -1,5 +1,4 @@
 # news.py
-# (C) 2013 Martijn Gonlag
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
